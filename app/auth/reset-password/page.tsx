9ebcd8607f6f2de54fe6cb983afcef93834c3f14
Camelus33 <camelus.tedin@gmail.com>
2025-05-18 22:17:11 +0900

    feat: add reset-password page

---
+++ app/auth/reset-password/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from 'react';
+
+const ResetPasswordPage = () => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
+      <h1>비밀번호 재설정</h1>
+      <p>비밀번호 재설정 폼이 여기에 표시됩니다.</p>
+      {/* TODO: 실제 비밀번호 재설정 폼 구현 */}
+    </div>
+  );
+};
+
+export default ResetPasswordPage; 
